fffa3237f89e3cf963ed6ed772bb93c033653efa
sweetylenka <sweetylenka2013@gmail.com>
2021-03-23 17:22:10 +0530

    Calculate Daily Employee Wage

--- EmployeeWageProblems/EmployeeWage.java
+++ EmployeeWageProblems/EmployeeWage.java
@@ -1,12 +1,21 @@
-public  class EmployeeWage {
+public class EmployeeWage {
+
 	public static void main(String[] args) {
-	//Constants
-	int IS_FULL_TIME = 1;
-	//Computation
-	double empCheck = Math.floor(Math.random() * 10) % 2;
-	if (empCheck == IS_FULL_TIME)
-		System.out.println("Employee is present");
-	else
-		System.out.println("Employee is absent");
+		// TODO Auto-generated method stub
+		//Constants
+		int IS_FULL_TIME=1;
+		int WAGE_PER_HR=20;
+
+		//Variable
+		int empHrs=0;
+		double empCheck = Math.floor(Math.random() * 10)%2;
+		if(empCheck == 1)
+			empHrs=8;
+		else
+			empHrs=0;
+		System.out.println("EmployeeWage : " + WAGE_PER_HR * empHrs);
+
 	}
+
 }
+
