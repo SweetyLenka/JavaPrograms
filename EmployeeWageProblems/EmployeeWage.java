8854c47a1cf8b9ffe1cbc5dbdeb437e8d8a0c38d
sweetylenka <sweetylenka2013@gmail.com>
2021-03-23 19:49:40 +0530

    Calculating Wages for a Month assuming 20 Working Days in a Month

--- EmployeeWageProblems/EmployeeWage.java
+++ EmployeeWageProblems/EmployeeWage.java
@@ -2,11 +2,13 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int WAGE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Variable
-		int empHrs=0;
+		int empHrs=0,totalEmpWage=0,empWage=0;
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
 		int empCheck = (int) Math.floor(Math.random() * 10)%3;
 		switch(empCheck) {
 		case IS_PART_TIME:
@@ -18,7 +20,10 @@ public class EmployeeWage {
 		default:
 			empHrs=0;
 		}
-		System.out.println("Employee Wage : " + WAGE_PER_HR * empHrs );
+		empWage=empHrs*WAGE_PER_HR;
+		totalEmpWage += empWage;
+		System.out.println("Employee Wage : " + empWage );
 	}
-
+	System.out.println("Total Employee Wage : "+ totalEmpWage);
+}
 }
